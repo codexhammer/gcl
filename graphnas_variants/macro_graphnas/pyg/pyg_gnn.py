abc6f0f564309bb98c4d7b5e008060b52636927e
codexhammer <sprakaraddi@gmail.com>
2021-11-14 20:05:26 +0800

    pyg_gnn pending

--- graphnas_variants/macro_graphnas/pyg/pyg_gnn.py
+++ graphnas_variants/macro_graphnas/pyg/pyg_gnn.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn.functional as F
 
-from gnn import GraphNet as BaseNet
+# from gnn import GraphNet as BaseNet
 from search_space import act_map
 from graphnas_variants.macro_graphnas.pyg.pyg_gnn_layer import GeoLayer
+import torch_geometric.nn as nn
 
 
 class GraphNet(torch.nn.Module):
 
-    def __init__(self, actions, num_feat, num_label, drop_out=0.6, multi_label=False, batch_normal=True, state_num=5,
+    def __init__(self, actions_mlp, num_feat, num_label, drop_out=0.6, multi_label=False, batch_normal=True, state_num_gcn=1,state_num_mlp=2,
                  residual=False):
         '''
         :param actions:
@@ -25,7 +26,7 @@ class GraphNet(torch.nn.Module):
         self.batch_normal = batch_normal
         
         # check structure of GNN
-        self.layer_nums = self.evalate_actions(actions, state_num)
+        self.layer_nums_mlp = self.evalate_actions(actions_mlp,state_num_mlp)
         self.head_num = 1
 
         # layer module
@@ -38,33 +39,37 @@ class GraphNet(torch.nn.Module):
             self.bns = torch.nn.ModuleList()
         self.layers = torch.nn.ModuleList()
         self.acts = []
-        self.build_hidden_layers(actions, batch_normal, drop_out, self.layer_nums, num_feat, num_label, state_num)
+        self.build_hidden_layers(actions, batch_normal, drop_out, self.layer_nums_mlp, num_feat, num_label, state_num)
 
-    def evalate_actions(self, actions, state_num):
-        state_length = len(actions)
-        if state_length % state_num != 0:
+    def evalate_actions(self, actions_mlp,state_num_mlp):
+        state_length_mlp = len(actions_mlp)
+        if state_length_mlp % state_num_mlp != 0:
             raise RuntimeError("Wrong Input: unmatchable input")
-        layer_nums = state_length // state_num
-        if self.evaluate_structure(actions, layer_nums, state_num=state_num):
+        layer_nums_mlp = state_length_mlp // state_num_mlp
+        if self.evaluate_structure(actions_mlp, layer_nums_mlp, state_num=state_num_mlp):
             pass
         else:
             raise RuntimeError("wrong structure")
-        return layer_nums
+        return layer_nums_mlp
 
-    def evaluate_structure(self, actions, layer_nums, state_num=6):
-        hidden_units_list = []
-        out_channels_list = []
-        for i in range(layer_nums):
-            out_channels = actions[i * state_num + 0]
-            hidden_units_list.append(self.head_num * out_channels)
-            out_channels_list.append(out_channels)
+    def evaluate_structure(self, actions_mlp, layer_nums_mlp, state_num=6):
+        # Check for output gnn == input mlp
+        pass
 
-        return out_channels_list[-1] == self.num_label
+
+        # hidden_units_list = []
+        # out_channels_list = []
+        # for i in range(layer_nums_mlp):
+        #     out_channels = actions[i * state_num + 0]
+        #     hidden_units_list.append(self.head_num * out_channels)
+        #     out_channels_list.append(out_channels)
+
+        # return out_channels_list[-1] == self.num_label
         
-    def build_hidden_layers(self, actions, batch_normal, drop_out, layer_nums, num_feat, num_label, state_num=6):
+    def build_hidden_layers(self, actions, batch_normal, drop_out, layer_nums_mlp, num_feat, num_label, state_num=6):
 
         # build hidden layer
-        for i in range(layer_nums):
+        for i in range(layer_nums_mlp):
 
             if i == 0:
                 in_channels = num_feat
@@ -78,7 +83,7 @@ class GraphNet(torch.nn.Module):
             # head_num = actions[i * state_num + 3]
             out_channels = actions[i * state_num + 0]
             concat = True
-            if i == layer_nums - 1:
+            if i == layer_nums_mlp - 1:
                 concat = False
             if self.batch_normal:
                 self.bns.append(torch.nn.BatchNorm1d(in_channels, momentum=0.5))
@@ -99,67 +104,4 @@ class GraphNet(torch.nn.Module):
                 if self.batch_normal:
                     output = self.bns[i](output)
 
-                output = act(layer(output, edge_index_all) + fc(output))
-        else:
-            for i, (act, layer) in enumerate(zip(self.acts, self.layers)):
-                output = F.dropout(output, p=self.dropout, training=self.training)
-                if self.batch_normal:
-                    output = self.bns[i](output)
-                output = act(layer(output, edge_index_all))
-        if not self.multi_label:
-            output = F.log_softmax(output, dim=1)
-        return output
-
-    def __repr__(self):
-        result_lines = ""
-        for each in self.layers:
-            result_lines += str(each)
-        return result_lines
-
-    @staticmethod
-    def merge_param(old_param, new_param, update_all):
-        for key in new_param:
-            if update_all or key not in old_param:
-                old_param[key] = new_param[key]
-        return old_param
-
-    def get_param_dict(self, old_param=None, update_all=True):
-        if old_param is None:
-            result = {}
-        else:
-            result = old_param
-        for i in range(self.layer_nums):
-            key = "layer_%d" % i
-            new_param = self.layers[i].get_param_dict()
-            if key in result:
-                new_param = self.merge_param(result[key], new_param, update_all)
-                result[key] = new_param
-            else:
-                result[key] = new_param
-        if self.residual:
-            for i, fc in enumerate(self.fcs):
-                key = f"layer_{i}_fc_{fc.weight.size(0)}_{fc.weight.size(1)}"
-                result[key] = self.fcs[i]
-        if self.batch_normal:
-            for i, bn in enumerate(self.bns):
-                key = f"layer_{i}_fc_{bn.weight.size(0)}"
-                result[key] = self.bns[i]
-        return result
-
-    def load_param(self, param):
-        if param is None:
-            return
-
-        for i in range(self.layer_nums):
-            self.layers[i].load_param(param["layer_%d" % i])
-
-        if self.residual:
-            for i, fc in enumerate(self.fcs):
-                key = f"layer_{i}_fc_{fc.weight.size(0)}_{fc.weight.size(1)}"
-                if key in param:
-                    self.fcs[i] = param[key]
-        if self.batch_normal:
-            for i, bn in enumerate(self.bns):
-                key = f"layer_{i}_fc_{bn.weight.size(0)}"
-                if key in param:
-                    self.bns[i] = param[key]
+                output = act(layer(outpu
