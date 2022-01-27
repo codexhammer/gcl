061ad01d7e4db2c84c0b44a749660404e60d0ac5
codexhammer <sprakaraddi@gmail.com>
2022-01-28 03:28:24 +0800

    ...

---
+++ utils/score.py
@@ -0,0 +1,12 @@
+import torch
+from sklearn.metrics import f1_score
+
+
+def evaluate(outputs, labels):
+    _, indices = torch.max(outputs, dim=1)
+    correct = torch.sum(indices == labels)
+    return correct.item() * 1.0 / len(labels)
+
+def f1_score_calc(outputs, labels):
+    _, indices = torch.max(outputs, dim=1)
+    return f1_score(labels.cpu().numpy(), indices.cpu().numpy(), average="micro")
